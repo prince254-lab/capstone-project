63fe78e7e4fc0409cd4853c4813bb4896fd11ff9
prince254-lab <palv8235@gmail.com>
2026-05-05 14:42:05 +0530

    upgraded ecommerce UI

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import Navbar from "../components/Navbar";
 import ProductCard from "../components/ProductCard";
 import Footer from "../components/Footer";
-import products from "../data/Products";
+import Products from "../data/Products";
 
 function Home() {
   return (
@@ -15,7 +15,7 @@ function Home() {
       </section>
 
       <div className="container">
-        {products.map((item) => (
+        {Products.map((item) => (
           <ProductCard key={item.id} product={item} />
         ))}
       </div>
